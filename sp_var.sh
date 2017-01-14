c6514b5ad4cf85704d90599cfe89f1817e30bee6
Yang-33win <kasai.yuta0810@gmail.com>
2017-01-15 00:48:18 +0900

    make special var.sh

---
+++ sp_var.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo $0 #script name
+echo $1 #1st argument
+echo $3 #3rd argument
+echo $# #the number of arguments
+echo $* # "all argument" 
+echo $@ # " each argument "
+
+
+<<EOF
+some special variables  
+EOF
