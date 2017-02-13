fb22d09f1d39217f013c2ad9d9756464905de464
Yang-33win <kasai.yuta0810@gmail.com>
2017-02-13 21:54:22 +0900

    improved cdup to be able to make files executable
    remove () and made file clear

--- cdup.sh
+++ cdup.sh
@@ -8,24 +8,29 @@ fi
 
 
 BASE="competitive-programming/"
-Purpose_directory=
-file_directory=
+Purpose_directory=""
+file_directory=""
+
 str=$1
 
 case "$str" in
-    ab ) ( Purpose_directory="atcoder/ABC/"; file_directory="atcoder/ABC_solve/ABC_solve/" ) ;;
-    ar ) ( Purpose_directory="atcoder/ARC/"; file_directory="atcoder/ARC_solve/ARC_solve/" ) ;;
-    ao ) ( Purpose_directory="AOJ_DPL/"; file_directory="AOJ_solved/AOJ_solved/" ) ;;
-    tdpc ) ( Purpose_directory="atcoder/TDPC/"; file_directory="atcoder/solve_TDPC_file/solve_TDPC_file/" ) ;; 
+    ab )   Purpose_directory="atcoder/ABC/"
+           file_directory="atcoder/ABC_solve/ABC_solve/"  ;;
+    ar )   Purpose_directory="atcoder/ARC/"
+           file_directory="atcoder/ARC_solve/ARC_solve/"  ;;
+    ao )   Purpose_directory="AOJ_DPL/"
+           file_directory="AOJ_solved/AOJ_solved/"  ;;
+    tdpc ) Purpose_directory="atcoder/TDPC/"
+           file_directory="atcoder/solve_TDPC_file/solve_TDPC_file/"  ;; 
     
-    * ) ( echo "there is no file such a file \"$1\" " );;
+    * )    echo "there is no file such a file \"$1\" " ;;
 esac
 
 echo $Purpose_directory
 
 cd /mnt/c/home
 
-#cp ${BASE}${file_directory}$2 ${BASE}${Purpose_directory}
+cp ${BASE}${file_directory}$2 ${BASE}${Purpose_directory}
 
 
 #to do 
