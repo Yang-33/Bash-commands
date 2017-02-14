9154412b91a885a74be1ca5b8c45ed180fc0078b
Yang-33win <kasai.yuta0810@gmail.com>
2017-02-14 18:24:52 +0900

    make filetime compare command

---
+++ filetimecomp.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# command file1 file2
+# if file1 is newer then file2 => 0(true) else 1(false)
+
+if [ $# -ne 2 ]; then
+    echo "Usage: command file1 file2" 1>&2
+    exit 1
+fi
+
+
+if [ ! -f $1 -o ! -f $2 ]; then
+    return 1
+fi
+
+
+if [ -n "`find $1 -newer $2 -print`" ]; then
+    return 0
+else
+    return 1
+fi
+
+
+
