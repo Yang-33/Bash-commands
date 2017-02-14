c8a495852a70cd9ac777bb1e998fdf1e84ad555d
Yang-33win <kasai.yuta0810@gmail.com>
2017-02-14 16:58:46 +0900

    wrote base

--- cdgf.sh
+++ cdgf.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
 
-#
-#
+# move to upload directory
+
+# sth -> atcoder/ABC
+
+BASE="competitive-programming"
+PURPOSE="atcoder/ABC"
+
+
+cd /mnt/c/home/${BASE}
+cd ${PURPOSE}
+
+exec /bin/bash
+
