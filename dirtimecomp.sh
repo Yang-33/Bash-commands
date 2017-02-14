0dff09c6fd2c88f59456b04599f69464326da6b5
Yang-33win <kasai.yuta0810@gmail.com>
2017-02-15 01:47:00 +0900

    Yeah!

--- dirtimecomp.sh
+++ dirtimecomp.sh
@@ -1,12 +1,56 @@
-#!/bin/sh
+#!/bin/bash
+#  . ./filetimecomp.sh
+
 
 # if one file is newer than timefile then 
 #   echo flename  else do nothing
 #
 
+time_comp(){
+
+
+if [ $# -ne 2 ]; then
+    echo "Usage: command file1 file2" 1>&2
+    exit 2
+fi
+
+
+if [ ! -f $1 -o ! -f $2 ]; then
+    exit 3
+fi
+
+
+if [ -n "`find $1 -newer $2 -print`" ]; then
+    return 0
+else
+    return 1
+fi
+
+}
+
+main(){
+
+STIME=`\find . -name *.TM `
+
+echo $STIME
+
+for File in `\find . -maxdepth 1 -type f`; do
+time_comp $File $STIME 
+STATUS=$?
+if [ "$STATUS" -eq 0 ]; then
+echo "Yeah!"
+fi
+#then echo "Yeah!"
+ #fi
+
+done
+
+
+}
 
 
 
+main $@
 
 ##
 
