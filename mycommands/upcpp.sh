b175238d46d886296b152955c959fa344664b57b
Yang-33 <kasai.yuta0810@gmail.com>
2017-05-08 09:32:39 +0900

    add projectoeular&others dir, bug fixed

--- mycommands/upcpp.sh
+++ mycommands/upcpp.sh
@@ -74,6 +74,7 @@ Set_TFile()
         "-$Optname_agc"   )              dir_name="AGC" ;;
         "-$Optname_hackerrank"  )        dir_name="HACKERRANK" ;;
         "-$Optname_joioi"   )            dir_name="JOI_IOI" ;;
+        "-$Optname_others"   )           dir_name="OTHERS" ;;
         "-$Optname_aoj"   )              dir_name="AOJ" ;;
         "-$Optname_poj"   )              dir_name="POJ" ;;
         "-$Optname_arc"  )               dir_name="ARC" ;;
@@ -127,22 +128,23 @@ opt_check()
     local re_opt=
 
     case  $1  in
-        "-$Optname_atcoder" ) re_opt="-$Optname_atcoder" ;;
-        "-$Optname_abc"  )  re_opt="-$Optname_abc"  ;;
-        "-$Optname_codeforces"  )  re_opt="-$Optname_codeforces"  ;;
-        "-$Optname_tdpc"  )  re_opt="-$Optname_tdpc"  ;;
-        "-$Optname_projecteular"  )  re_opt="-$Optname_projecteular"  ;;
-        "-$Optname_codechef"  )  re_opt="-$Optname_codechef"  ;;
-        "-$Optname_agc"  )  re_opt="-$Optname_agc"  ;;
-        "-$Optname_hackerrank"  )  re_opt="-$Optname_hackerrank"  ;;
-        "-$Optname_joioi"  )  re_opt="-$Optname_joioi"  ;;
-        "-$Optname_aoj"  )  re_opt="-$Optname_aoj"  ;;
-        "-$Optname_poj"  )  re_opt="-$Optname_poj"  ;;
-        "-$Optname_arc"  )  re_opt="-$Optname_arc"  ;;
-        "-$Optname_srm"  )  re_opt="-$Optname_srm"  ;;
-        "-$Optname_topcoder"  )  re_opt="-$Optname_topcoder"  ;;
-        "-$Optname_uva"  )  re_opt="-$Optname_uva"  ;;
-        "-$Optname_yukicoder"  )  re_opt="-$Optname_yukicoder"  ;;
+        "-$Optname_atcoder" ) 				re_opt="-$Optname_atcoder" ;;
+        "-$Optname_abc"  )  				re_opt="-$Optname_abc"  ;;
+        "-$Optname_codeforces"  )  			re_opt="-$Optname_codeforces"  ;;
+        "-$Optname_tdpc"  )  				re_opt="-$Optname_tdpc"  ;;
+        "-$Optname_projecteular"  )  		re_opt="-$Optname_projecteular"  ;;
+        "-$Optname_codechef"  )             re_opt="-$Optname_codechef"  ;;
+        "-$Optname_agc"  )  				re_opt="-$Optname_agc"  ;;
+        "-$Optname_hackerrank"  ) 		    re_opt="-$Optname_hackerrank"  ;;
+        "-$Optname_joioi"  )			    re_opt="-$Optname_joioi"  ;;
+        "-$Optname_others"  ) 			    re_opt="-$Optname_others"  ;;
+        "-$Optname_aoj"  ) 				    re_opt="-$Optname_aoj"  ;;
+        "-$Optname_poj"  ) 				    re_opt="-$Optname_poj"  ;;
+        "-$Optname_arc"  ) 				    re_opt="-$Optname_arc"  ;;
+        "-$Optname_srm"  )  				re_opt="-$Optname_srm"  ;;
+        "-$Optname_topcoder"  ) 		    re_opt="-$Optname_topcoder"  ;;
+        "-$Optname_uva"  ) 				    re_opt="-$Optname_uva"  ;;
+        "-$Optname_yukicoder"  ) 		    re_opt="-$Optname_yukicoder"  ;;
         * )   echo "There is no presence such a option " $1
             echo "Usege: (on matched dir) opt_check [-option]"
             echo "Error on opt_check func."
@@ -210,7 +212,8 @@ cd_to_upload_dir()
         "-$Optname_agc" )   _purpose=${Pass_agc_u}               ;;
         "-$Optname_hackerrank" )   _purpose=${Pass_hackerrank_u}               ;;
         "-$Optname_joioi" )   _purpose=${Pass_joioi_u}             ;;
-        "-$Optname_aoj" )   _purpose=${Pass_aoj_u}               ;;
+        "-$Optname_others" )   _purpose=${Pass_others_u}               ;;
+		"-$Optname_aoj" )   _purpose=${Pass_aoj_u}               ;;
         "-$Optname_poj" )   _purpose=${Pass_poj_u}               ;;
         "-$Optname_arc" )   _purpose=${Pass_arc_u}               ;;
         "-$Optname_srm" )   _purpose=${Pass_srm_u}             ;;
@@ -262,7 +265,7 @@ copyfile()
     # --option
 
 
-    while  getopts  abcdefghjoprstuy  OPT ## you have to write all potion here
+    while  getopts  abcdfghjloprstuyz  OPT ## you have to write all potion here
     do 
         case  ${OPT}  in  #2nd char is available
             "$Optname_atcoder"      )   purpose_directory=${Pass_atcoder_u}  
@@ -282,6 +285,8 @@ copyfile()
             "$Optname_hackerrank"   )   purpose_directory=${Pass_hackerrank_u}        
             ;;
             "$Optname_joioi"        )   purpose_directory=${Pass_joioi_u}             
+            ;;
+			"$Optname_others"       )   purpose_directory=${Pass_others_u}               
             ;;
             "$Optname_aoj"          )   purpose_directory=${Pass_aoj_u}               
             ;;
@@ -387,7 +392,7 @@ tmfile_update()
 
 main()
 {
- export SLACKIFY_TOKEN=
+ export SLACKIFY_TOKEN=" "
 
     Exp="*.cpp"
     Ext="cpp"
@@ -399,7 +404,7 @@ main()
    # for windows 
     Home="/mnt/c/home/"
    # for ubuntu
-   # Home="/media/yang33/F0E6928BE6925228/home/"
+   # Home="/media/yang33/a/home/"
     Base="competitive-programming/"
     Origin="solve/"
    # pass & option
@@ -416,9 +421,6 @@ main()
     Pass_tdpc_u="atcoder/TDPC/"
     Optname_tdpc="d"
 
-    Pass_projecteular_u="ProjectEular/"
-    Optname_projecteular="e"
-
     Pass_codechef_u="Codechef/"
     Optname_codechef="f"
 
@@ -430,16 +432,19 @@ main()
 
     Pass_joioi_u="JOI_IOI/"
     Optname_joioi="j"
-    
+
+    Pass_projecteular_u="ProjectEular/"
+    Optname_projecteular="l"
+
     Pass_aoj_u="AOJ/"
     Optname_aoj="o"
 
     Pass_poj_u="POJ/"
     Optname_poj="p"
-    
+
     Pass_arc_u="atcoder/ARC/"
     Optname_arc="r"
-    
+
     Pass_srm_u="topcoder/SRM/"
     Optname_srm="s"
 
@@ -448,30 +453,51 @@ main()
 
     Pass_uva_u="Uva/"
     Optname_uva="u"
-    
+ 
     Pass_yukicoder_u="Yukicoder/"
     Optname_yukicoder="y"
-##########################################
+    
+    Pass_others_u="Others/"
+    Optname_others="z"
+	##########################################
+    
     
     local cur_dir=`pwd`
     local _option=
     local status=
     local count=0    
-
+    local onefile_flag="FALSE"
     # opt check and decision
     #if [ $# -eq 0 ]
     #then 
     #    _option=`which_dir $cur_dir `
     #    
     #el
-    if [ $# -eq 1 ]
-    then #opt check
-        _option=`opt_check $1 `
-    else
-        echo "Usege: $Command_name [-option]"
+
+    if [ $1 = "--help" ]
+    then
+        echo "Usage: $Command_name -DIRnameoption [ [-of a FILE] or [-cd] ]"
         exit 1
     fi
 
+
+    if [ $# -eq 1 ]
+    then #opt check
+        _option=`opt_check $1 `
+    elif [ $# -eq 3 -a $2 = "-of" ]
+    then 
+        _option=`opt_check $1`
+        onefile_flag="TRUE"
+    elif [ $# -eq 2 -a $2 = "-cd" ]
+    then
+        _option=`opt_check $1`
+        cd_to_upload_dir -u $_option
+        exit 1
+    else 
+        echo "Usege: $Command_name [-option]"
+        exit 1
+    fi
+	
     
     f_message $_option
     
@@ -483,21 +509,25 @@ main()
 
     cd_to_upload_dir -v $_option
 
-
-    for File in `\find . -maxdepth 1 -type f -name "${Exp}" `
-    do
-        time_comp $File $tm_file 
-        status=$?
-        
-        if [ "$status" -eq 0 ]
-        then
-            (  copyfile $_option $File   ) 
+    if [ $onefile_flag = "TRUE" ]
+    then
+         (  copyfile $_option $3   ) 
             count=$((count+1))
-        fi
+    else
+        for File in `\find . -maxdepth 1 -type f -name "${Exp}" `
+        do
+            time_comp $File $tm_file 
+            status=$?
+        
+            if [ "$status" -eq 0 ]
+            then
+                (  copyfile $_option $File   ) 
+                count=$((count+1))
+            fi
         
         
-    done
-
+        done
+    fi
 
     if [ $count -eq 0 ]
     then 
@@ -514,8 +544,10 @@ main()
 
     tmfile_update
 
-    cd_to_upload_dir -u $_option
-    
+    if [ $onefile_flag = "FALSE" ]
+    then
+        cd_to_upload_dir -u $_option
+    fi
 }
 
 main $@
